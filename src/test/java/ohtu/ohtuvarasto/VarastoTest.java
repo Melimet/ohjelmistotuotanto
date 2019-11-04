c6588ad9d485c5d19039cede885aab78fb2681a6
Erkka Rahikainen <Erkka.Rahikainen@gmail.com>
2019-11-04 20:02:19 +0200

    Täysi testikattavuus varasto-luokassa

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -65,4 +65,74 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+    public void eiNegatiivistaTilavuutta(){
+        varasto = new Varasto(-3214);
+
+
+        assertEquals(varasto.getTilavuus(), 0.0, vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivinenLisaysJaOtto(){
+        varasto.lisaaVarastoon(-5);
+        assertEquals(varasto.getSaldo(), 0, vertailuTarkkuus);
+        varasto.otaVarastosta(-5);
+        assertEquals(varasto.getSaldo(), 0, vertailuTarkkuus);
+
+    }
+
+    @Test
+    public void saldoEiYlity(){
+
+        varasto.lisaaVarastoon(varasto.getTilavuus() + 1);
+
+        assertEquals(varasto.getSaldo(), varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+
+    @Test
+    public void tulostusTesti(){
+        varasto.lisaaVarastoon(1);
+        assertEquals(varasto.toString(),("saldo = 1.0, vielä tilaa 9.0"));
+    }
+
+    @Test
+    public void alustusMolemmillaOikeillaArvoillaSkulaa(){
+        varasto = new Varasto(12, 6);
+
+        assertEquals(varasto.getTilavuus(), 12, vertailuTarkkuus);
+        assertEquals(varasto.getSaldo(), 6, vertailuTarkkuus);
+    }
+
+    @Test
+    public void alustaessaNegatiivisetArvotKorjataan(){
+
+        varasto = new Varasto(-1, -2);
+
+        assertEquals(varasto.getTilavuus(), 0, vertailuTarkkuus);
+        assertEquals(varasto.getSaldo(), 0, vertailuTarkkuus);
+ 
+    }
+
+    @Test
+    public void varastostaLahteeKaikkiMahdollinen(){
+
+        varasto.lisaaVarastoon(3);
+
+        assertEquals(varasto.otaVarastosta(4),3, vertailuTarkkuus);
+
+    }
+
+    @Test
+    public void alustusYlijaamallaToimii(){
+        varasto = new Varasto(2, 4);
+
+
+
+        assertEquals(varasto.getTilavuus(), 2, vertailuTarkkuus);
+        assertEquals(varasto.getSaldo(), 2, vertailuTarkkuus);
+
+
+    }
 }
